406492ccacc927abec779faad851f0baad11eead
DuduFN <dudu.fn1364@gmail.com>
2022-11-23 10:46:59 -0300

    Possible Rook moves

--- ChessMatch/src/main/java/org/example/application/Program.java
+++ ChessMatch/src/main/java/org/example/application/Program.java
@@ -1,9 +1,11 @@
 package org.example.application;
 
+import org.example.chess.ChessException;
 import org.example.chess.ChessPiece;
 import org.example.chess.ChessPosition;
 import org.example.chess.Match;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,16 +15,27 @@ public class Program {
         Match match = new Match();
 
         while (true) {
-            UI.printBoard(match.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(match.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = match.peformChessMove(source, target);
+                ChessPiece capturedPiece = match.performChessMove(source, target);
+            }
+            catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
--- ChessMatch/src/main/java/org/example/chess/Match.java
+++ ChessMatch/src/main/java/org/example/chess/Match.java
@@ -25,10 +25,11 @@ public class Match {
         return mat;
     }
 
-    public ChessPiece peformChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -44,7 +45,18 @@ public class Match {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position");
         }
+        if (!board.piece(position).isThereAnyPossibleMovie()) {
+            throw new ChessException("There is no possible moves for the chosen piece");
+        }
     }
+
+    private void validateTargetPosition(Position source, Position target){
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+    }
+
+
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
