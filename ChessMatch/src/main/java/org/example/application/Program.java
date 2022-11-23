82e2eb64284795b373dc02543244f07dd3a5273a
DuduFN <dudu.fn1364@gmail.com>
2022-11-23 09:33:59 -0300

    Moving pieces

--- ChessMatch/src/main/java/org/example/application/Program.java
+++ ChessMatch/src/main/java/org/example/application/Program.java
@@ -1,11 +1,28 @@
 package org.example.application;
 
+import org.example.chess.ChessPiece;
+import org.example.chess.ChessPosition;
 import org.example.chess.Match;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         Match match = new Match();
-        UI.printBoard(match.getPieces());
+
+        while (true) {
+            UI.printBoard(match.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = match.peformChessMove(source, target);
+        }
     }
 }
--- ChessMatch/src/main/java/org/example/chess/Match.java
+++ ChessMatch/src/main/java/org/example/chess/Match.java
@@ -1,6 +1,7 @@
 package org.example.chess;
 
 import org.example.boardgame.Board;
+import org.example.boardgame.Piece;
 import org.example.boardgame.Position;
 import org.example.chess.pieces.King;
 import org.example.chess.pieces.Rook;
@@ -24,13 +25,44 @@ public class Match {
         return mat;
     }
 
+    public ChessPiece peformChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target){
+        Piece p = board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece(p, target);
+        return capturedPiece;
+    }
+
+    private  void validateSourcePosition(Position position){
+        if(!board.thereIsAPiece(position)){
+            throw new ChessException("There is no piece on source position");
+        }
+    }
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
     private void initialSetup() {
-        placeNewPiece('b',6, new Rook(board, Color.WHITE));
-        placeNewPiece('e',8, new King(board, Color.BLACK));
-        placeNewPiece('e',1, new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
+
